58794642fe6b3c990f2e09ef71b130924e3075a2
unknown <22560004@stmik.jayakarta.ac.id>
2024-05-11 20:56:10 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+// buat konfigurasi koneksi
+const koneksi = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'dbmovie',
+   multipleStatements: true
+});
+// koneksi database
+koneksi.connect((err) => {
+   if (err) throw err;
+   console.log('MySQL Connected...');
+});
+module.exports = koneksi;
